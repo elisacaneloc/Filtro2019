540f8363bf6219b3b7c13751321509bc66d4378f
elisacaneloc <elisacaneloc@gmail.com>
2019-01-09 11:48:48 -0300

    ahora allconduct y all displace son array normales
    Ahora se puede llamar a los datos de cada traza como [0,traza] recordamos que van desde 0 a 4999

--- basic.py
+++ basic.py
@@ -15,3 +15,14 @@ Leemos los archivos .mat para trabajar con ellos como array de scipy
 
 allconduct = sp.io.loadmat('allConduct.mat')
 alldisplace = sp.io.loadmat('allDisplace.mat')
+
+'''
+Ahora seleccionamos del archivo solo la parte que nos interesa
+pues si uno ve el contenido de este, vera que hay muchas cosas
+que no nos sirven, con esto podremos usar su contenido como
+un array normal, recordemos que en python la idenxacion parte en 0
+por lo tanto tenemos desde la traza 0 (1) hasta la 4999 (5000)
+'''
+
+allconduct = allconduct['allConduct']
+alldisplace =  alldisplace['allDisplace']
