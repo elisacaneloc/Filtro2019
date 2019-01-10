aadd30c481d22c238d9d0bedd9a7062ef0b65521
elisacaneloc <elisacaneloc@gmail.com>
2019-01-10 15:37:34 -0300

    Update Datos.py
    Se leen datos y se crean listas para los datos y y x

--- Datos.py
+++ Datos.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import numpy as np
+import scipy as sp
+from scipy import io
+
+'''
+Este programa está pensado para trabajar con los datos del archivo curves.mat
+La idea es poder hacer lo mismo que está hecho en el programa de matlab pero
+pero en python.
+'''
+
+curve = sp.io.loadmat('curves.mat')
+curve_cell = curve['curva_cell']
+
+todos_y = []
+for i in range(0,5000):
+    datos = curve[0,i]
+    d = datos[0][0]
+    y = d[1]
+    todos_y.append(y)
+
+todos_x = []
+for i in range(0,5000):
+    datos = curve[0,i]
+    d = datos[0][0]
+    x = d[1]
+    todos_x.append(x)
