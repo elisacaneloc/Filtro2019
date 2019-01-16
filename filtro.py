c263b555e885d370d301d648c692694a02524781
elisacaneloc <elisacaneloc@gmail.com>
2019-01-16 20:10:21 -0300

    Update filtro.py
    Se crea función reducir, que reduce los arreglos para quedarnos con el rango que me interesa ( 0,2)

--- filtro.py
+++ filtro.py
@@ -14,3 +14,29 @@ from scipy.optimize import curve_fit
 def model(x,a,b,c,d):
     return a*np.exp(-x*b + d) + c
 
+# copiemos para uno y después lo arreglamos para todos
+# y despues hacemos función.
+    
+
+def reducir(D_i,C_i):
+    x_data = D_i
+    y_data = C_i
+    n = len(x_data)
+    data = np.zeros(n,2)
+    for i in range(n):
+        data[i][0] = x_data[i]
+        data[i][1] = y_data[i]
+    
+    data_quiero = []
+    for i in range(n):
+        if 2>data[i][0]>0:
+            data_quiero.append(data[i])
+    data_quiero = np.array(data_quiero)
+    s = len(data_quiero)
+    y_data_quiero = np.zeros(s)
+    x_data_quiero = np.zeros(s)
+    for i in range(s):
+        x_data_quiero[i] = data_quiero[i][0]
+        y_data_quiero[i] = data_quiero[i][1]
+    return x_data_quiero, y_data_quiero
+
