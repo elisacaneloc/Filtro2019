0ac71ffdaea117972b7b044170f2c10edb107275
elisacaneloc <elisacaneloc@gmail.com>
2019-01-16 19:53:14 -0300

    Create filtro.py
    Se crea .py que tendrá el filtro

---
+++ filtro.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 16 15:23:44 2019
+
+@author: Elisa Canelo
+"""
+
+import medicion as md
+import numpy as np
+from scipy.stats import kstest
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+
